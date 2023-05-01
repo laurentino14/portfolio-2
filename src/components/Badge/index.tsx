9e50c13a492f547fbdcfa508e57dc744bee35b51
Lucas Laurentino <laurentino14@outlook.com>
2023-05-01 14:58:44 -0300

    fix match

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -5,11 +5,11 @@ export function Badge({tag}: {tag: string}) {
     <Link
       href={`/articles#${tag}`}
       className={`${
-        tag.match('react')
+        tag === 'react'
           ? 'bg-primary'
-          : tag.match('nodejs')
+          : tag === 'nodejs'
           ? 'bg-green-500 '
-          : ''
+          : 'bg-cyan-500'
       } hover:brightness-110 transition text-white font-semibold px-4 py-1 rounded-full`}>
       {tag}
     </Link>
